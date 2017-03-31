33e57620d6a59af02bf5231e4cd233c3a972f3da
Louis Ritter <louritter1@gmail.com>
2017-03-31 00:08:43 -0400

    Added Logo and Routing

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Louis Ritter | Web Developer' });
 });
+router.get('/about', function (req, res) {
+  res.render('about');
+});
+router.get('/portfolio', function (req, res) {
+  res.render('portfolio');
+});
+router.get('/contact', function (req, res) {
+  res.render('contact');
+});
+
 
 module.exports = router;
