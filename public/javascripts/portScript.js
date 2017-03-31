8df4c91f5e03192dd2484e3fee77b1df46e7c41c
Louis Ritter <louritter1@gmail.com>
2017-03-31 14:37:40 -0400

    port page

---
+++ public/javascripts/portScript.js
@@ -0,0 +1,18 @@
+$( document ).ready(function(){
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});  
+// Open
+$('.gallery-expand').galleryExpand('open');
+
+// Close
+$('.gallery-expand').galleryExpand('close');
+
+
+});
